a883d022b27830c458808f7fb1a0967209325c44
Jeff Bezos Stonerbot <cynosure@disroot.org>
2021-10-27 15:16:50 -0600

    Created robot.h for the Neos
    Basic info:
    The following files were changed:
    robot.h
    
    I made a robot.h file that gave IDs to the Neos and assigned them to encoders. It is just repurposed 2021 Infinite recharge code so it should work. The falcons are not done yet.
    
    -- Commit made by Jeff Bezos Stonerbot (AKA cynosure or Cynosure-NUL) <cynosure@disroot.org)
    My PGP key can be found at https://cynosure.neocities.org/about
    Fly safe
     On branch robot.h
     Changes to be committed:
    	new file:   README.md
    	modified:   src/main/include/Robot.h

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -7,6 +7,10 @@
 #include <string>
 
 #include <frc/TimedRobot.h>
+#include <frc/Joystick.h>
+#include <frc/WPILib.h>
+#include "rev/CANSparkMax.h"
+#include <frc/drive/DifferentialDrive.h>
 #include <frc/smartdashboard/SendableChooser.h>
 
 class Robot : public frc::TimedRobot {
@@ -24,7 +28,38 @@ class Robot : public frc::TimedRobot {
 
  private:
   frc::SendableChooser<std::string> m_chooser;
-  const std::string kAutoNameDefault = "Default";
-  const std::string kAutoNameCustom = "My Auto";
-  std::string m_autoSelected;
+  // Miscellaneous Motor IDs
+  // Neos only, Falcons are somewhere else
+  const std::int armMotorID = 1;
+  const std::int grabberMotorID = 2;
+  const std::int leftClimbMotorID = 3;
+  const std::int rightClimbMotorID = 4;
+
+  //TODO: DRIVE CODE
+
+  //"Joystick"
+  frc::Joystick m_stick{0};
+  const std::int leftControlStick = 1; //used to move
+  const std::int rightControlStick = 4; //used to rotate
+
+  //Binding motors to controllers, season one, episode four
+  //Neo motors
+  rev::CANSparkMax m_armMotor{armMotorID, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_grabberMotor{grabberMotorID, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_leftClimbMotor{leftClimbMotorID, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_rightClimbMotor{rightClimbMotorID, rev::CANSparkMax::MotorType::kBrushless};
+
+  rev::CANEncoder m_armEncoder = m_armMotor.GetEncoder();
+  rev::CANEncoder m_grabberEncoder = m_grabberMotor.GetEncoder();
+  rev::CANEncoder m_leftClimbEncoder = m_leftClimbMotor.GetEncoder();
+  rev::CANEncoder m_rightClimbEncoder = m_rightClimbMotor.GetEncoder();
+
+  // Limelight
+  //Copied from 2021
+  std::shared_ptr<NetworkTable> m_table = nt::NetworkTableInstance::GetDefault().GetTable("limelight-scorpio");
+  double tx_OFFSET = 0.0; // old = 3.0
+
+
+
 };
+
